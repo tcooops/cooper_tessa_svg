26c7b4ec582b541c05a199e063212557c29a1f7b
Tessa Benton-Cooper <tbc@Tessas-MacBook-Pro.local>
2020-01-24 19:54:05 -0500

    inserted icons and basic javascript interaction

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(() => {
+	// this is a module pattern, also called iife. Query selector does the same thing like in CSS where it grabs what you're asking for in the html
+	const mySpan = document.querySelector('main span'),
+		  bananaSVG = document.querySelector('.bananaLeaf');
+		  bikiniSVG = document.querySelector('.bikini');
+		  cactusSVG = document.querySelector('.cactus');
+		  cameraSVG = document.querySelector('.camera');
+		  drinkSVG = document.querySelector('.drink');
+		  pineappleSVG = document.querySelector('.pineapple');
+		  planeSVG = document.querySelector('.plane');
+		  shellSVG = document.querySelector('.shell');
+		  starfishSVG = document.querySelector('.starfish');
+		  sunSVG = document.querySelector('.sun');
+		  treeSVG = document.querySelector('.tree');
+
+
+	function toggleSelected() {
+		// we want to add some kind of indicator that someone has selected an icon
+
+		//debugger;
+		this.classList.toggle('selected');
+	}
+
+
+	// set up our triggers here. A click is something you do with the mouse... same with a page reload or form submission, etc.
+	mySpan.addEventListener('click', toggleSelected);
+	bananaSVG.addEventListener('click', toggleSelected);
+	bikiniSVG.addEventListener('click', toggleSelected);
+	cactusSVG.addEventListener('click', toggleSelected);
+	cameraSVG.addEventListener('click', toggleSelected);
+	drinkSVG.addEventListener('click', toggleSelected);
+	pineappleSVG.addEventListener('click', toggleSelected);
+	planeSVG.addEventListener('click', toggleSelected);
+	shellSVG.addEventListener('click', toggleSelected);
+	starfishSVG.addEventListener('click', toggleSelected);
+	sunSVG.addEventListener('click', toggleSelected);
+	treeSVG.addEventListener('click', toggleSelected);
+
+
+})();
+
